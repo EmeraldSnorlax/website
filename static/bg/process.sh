2f359d0cd245bb98e4788d8a246c49a915c288be
Rain <emeraldsnorlax@disroot.org>
2024-02-06 14:52:21 +0000

    240 only

--- static/bg/process.sh
+++ static/bg/process.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-rm 240.apng 480.apng
+rm 240.apng
 ffmpeg -i pexels_videos_1531416\ \(1080p\).mp4 -vf "scale=240:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=12:reserve_transparent=0[p];[s1][p]paletteuse=dither=sierra2_4a,select=not(mod(n\,3))" -vsync vfr -plays 0 240.apng
-ffmpeg -i pexels_videos_1531416\ \(1080p\).mp4 -vf "scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=12:reserve_transparent=0[p];[s1][p]paletteuse=dither=sierra2_4a,select=not(mod(n\,3))" -vsync vfr -plays 0 480.apng
