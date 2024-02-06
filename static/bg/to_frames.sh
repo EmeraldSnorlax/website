7e1e3ade69dcf7766901d0dfc857380452d63e57
Rain <emeraldsnorlax@disroot.org>
2024-02-06 14:48:19 +0000

    two sizes of apng, change dither alg

--- static/bg/to_frames.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-ffmpeg -i pexels_videos_1531416\ \(1080p\).mp4 -vf "scale=240:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=12:reserve_transparent=0[p];[s1][p]paletteuse=dither=bayer,select=not(mod(n\,4))" -vsync vfr %03d.png
-mkdir frames
-mv *.png frames/
-oxipng -o max --strip safe -Z --scale16 --alpha --recursive frames
