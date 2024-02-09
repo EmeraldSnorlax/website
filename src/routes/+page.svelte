b3e87a258fe99648a5cbd51482f82b01c5403244
Rain <emeraldsnorlax@disroot.org>
2024-02-09 17:37:59 +0000

    crt grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,91 @@
-<script>
+<script lang="ts">
 	import Crt from '$lib/Crt.svelte';
-	import Rain from '$lib/Rain.svelte';
+
+	import { loadParts } from '$lib/avatar/parts';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { draw } from '$lib/avatar';
+
+	let rain: HTMLCanvasElement;
+	let fragments: HTMLCanvasElement[][] = [];
+
+	if (browser) {
+		onMount(async () => {
+			fragments = [
+				[
+					document.createElement('canvas'),
+					document.createElement('canvas'),
+					document.createElement('canvas')
+				],
+				[
+					document.createElement('canvas'),
+					document.createElement('canvas'),
+					document.createElement('canvas')
+				],
+				[
+					document.createElement('canvas'),
+					document.createElement('canvas'),
+					document.createElement('canvas')
+				]
+			];
+			if (!rain) return;
+			const parts = await loadParts();
+			const fullCtx = rain.getContext('2d');
+			if (!fullCtx) return;
+			draw(fullCtx, [
+				parts.body,
+				parts.hair,
+				parts['eyes-011'],
+				parts['mouth-009'],
+				parts['eyebrows-001'],
+				parts['halo-003'],
+				parts['outfit-018'],
+				parts.glasses,
+				parts.shadow
+			]);
+			fragments.forEach((row, y) => {
+				row.forEach((fragment, x) => {
+					const ctx = fragment.getContext('2d');
+					if (!ctx) return;
+					ctx.drawImage(rain, x * 200, y * 200 - 50, 200, 200, 0, 0, 200, 200);
+				});
+			});
+		});
+	}
 </script>
 
 <svelte:head>
 	<link rel="preload" media="not (prefers-reduced-motion)" as="image" href="/bg/240.apng" />
 </svelte:head>
 
-<main class="min-h-screen px-8">
-	<div class="grid scale-50 grid-cols-4 grid-rows-3">
-		<div class="col-start-2 row-start-1 translate-y-8"><Crt /></div>
-		<div class="col-start-3 row-start-1 translate-x-4"><Crt /></div>
+<canvas bind:this={rain} width={600} height={600} class="hidden aspect-square w-full" />
 
-		<div class="col-start-1 row-start-2 translate-y-9 -translate-x-3"><Crt /></div>
-		<div class="col-start-2 row-start-2"><Crt><Rain /></Crt></div>
-		<div class="col-start-3 row-start-2 translate-x-2 -translate-y-12"><Crt /></div>
-
-		<div class="row-start-3 -translate-x-4 "><Crt /></div>
-		<div class="col-start-2 row-start-3 -translate-x-3 -translate-y-8"><Crt /></div>
-		<div class="col-start-3 row-start-3 -translate-y-12 translate-x-4"><Crt /></div>
-
-	</div>
-</main>
+{#if browser}
+	<main class="min-h-screen px-8">
+		{#each fragments as row, y}
+			<div class="flex">
+				{#each row as fragment, x}
+					<div class="w-1/3">
+						{#if (x === 0 && y === 1) || (x === 2 && y ===2)}
+						<!-- cool static crts :)-->
+						<Crt />
+						{:else if !(x === 2 && y === 0)}
+						<!-- the rain fragments-->
+								<Crt>
+									<canvas
+										bind:this={fragment}
+										width={200}
+										height={200}
+										class="aspect-square w-full"
+									/>
+								</Crt>
+						{/if}
+					</div>
+				{/each}
+			</div>
+		{/each}
+	</main>
+{/if}
 
 <style lang="postcss">
 	@media not (prefers-reduced-motion) {
@@ -30,5 +94,8 @@
 			image-rendering: pixelated;
 			@apply bg-cover;
 		}
+	}
+	canvas {
+		@apply aspect-square w-full;
 	}
 </style>
