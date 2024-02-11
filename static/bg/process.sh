c88950722f8ade75fcb49c99bf49243293f85a5f
Rain <emeraldsnorlax@disroot.org>
2024-02-11 23:06:40 +0000

    shave a few more kb

--- static/bg/process.sh
+++ static/bg/process.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm 240.apng
-ffmpeg -i pexels_videos_1531416\ \(1080p\).mp4 -vf "scale=180:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=9:reserve_transparent=0[p];[s1][p]paletteuse=dither=sierra2_4a,select=not(mod(n\,3))" -vsync vfr -plays 0 180.apng
+rm 180.apng
+ffmpeg -i pexels_videos_1531416\ \(1080p\).mp4 -vf "scale=180:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=9:reserve_transparent=0[p];[s1][p]paletteuse=dither=sierra2_4a,select=not(mod(n\,3))" -vsync vfr -plays 0 -ss 0 -t 6 180.apng
