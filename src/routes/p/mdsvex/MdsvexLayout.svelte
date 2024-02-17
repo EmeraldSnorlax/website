a26188e2bd4f09162986b9faa188051cd99e6451
Rain <emeraldsnorlax@disroot.org>
2024-02-17 15:01:51 +0000

    dark theme

--- src/routes/p/mdsvex/MdsvexLayout.svelte
+++ src/routes/p/mdsvex/MdsvexLayout.svelte
@@ -27,11 +27,25 @@
 
 <script lang="ts">
 	import dayjs from 'dayjs';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 	// mdsvex is so cool for not supporting typescript
 	// see App.Post in src/app.d.ts for the shape of a post...
 	let post = { ...$$props };
 	const unfuckedDate = new Date(Date.parse(post.date));
 	post.date = unfuckedDate; // mdsvex is double cool for reading the frontmatter date as a string
+	let theme: 'light' | 'dark';
+
+	if (browser) {
+		theme =
+			(localStorage.getItem('theme') as 'light' | 'dark') ||
+			(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+	}
+	$: (() => {
+		if (browser) {
+			localStorage.setItem('theme', theme);
+		}
+	})();
 </script>
 
 <svelte:head>
@@ -59,89 +73,87 @@
 	/>
 </svelte:head>
 
-<div
-	class="mx-auto max-w-[160ch] border-2 border-neutral-300 bg-slate-500 dark:border-neutral-400 dark:bg-slate-800 sm:mt-16"
->
-	<div class="bg-neutral-300 px-2 pt-0.5 font-sans text-2xl dark:bg-neutral-800 sm:text-3xl">
-		<a
-			aria-label="Back to post list"
-			id="back-to-post-list"
-			class="-me-0.5 inline-block text-neutral-700 hover:text-blue-700 dark:text-neutral-400"
-			href="/p"
-			><span aria-hidden="true">/</span><span class="font-normal underline underline-offset-2"
-				>posts</span
-			><span aria-hidden="true">/</span></a
-		>
-		<span class="-ms-0.5 inline-block font-bold lowercase dark:text-neutral-400" id="slug"
-			>{post.slug}</span
-		>
-	</div>
-
-	<div class="flex justify-between bg-neutral-400 px-2 dark:bg-neutral-700">
-		<header>
-			<div class="flex w-full items-center text-lg">
-				<PixelarticonsInfoBox class="me-2 h-4 w-4 text-neutral-700" role="presentation" />
-				<div class="mt-1">
-					<span class="-mb-3 text-neutral-800 dark:text-neutral-200"
-						>Created: <time
-							class="font-bold text-neutral-900 dark:text-neutral-200"
-							datetime={dayjs(unfuckedDate).format('YYYY-MM-DD')}
-							>{dayjs(unfuckedDate).format('YYYY-MMM-DD')}</time
-						></span
-					>
-					<ul aria-label="post tags" class="flex">
-						{#if post.tags}
-							{#each post.tags as tag}
-								<li class="me-1 flex items-center text-lg text-neutral-900 dark:text-neutral-300">
-									<PixelarticonsLabel
-										role="presentation"
-										class="me-0.5 h-3 w-3 text-neutral-500 dark:text-neutral-400"
-									/><span>{tag}</span>
-								</li>
-							{/each}
-						{/if}
-					</ul>
-				</div>
-			</div>
-		</header>
-		<div class="text-neutral-800 dark:text-neutral-200">
-			<label>
-				Theme:
-				<select class="dark:bg-neutral-500">
-					<option value="light">Light</option>
-					<option value="dark">Dark</option>
-				</select>
-			</label>
-			<label class="flex">
-				Film grain:
-				<input type="checkbox" class="ms-2" checked />
-			</label>
-		</div>
-	</div>
-	<!-- page margin ruler type thingy, purely decorative. -->
+<div class={theme}>
 	<div
-		aria-hidden="true"
-		class="hidden h-3 border-t-2 border-neutral-600 bg-neutral-300 dark:invert lg:block"
+		class={`mx-auto max-w-[160ch] border-2 border-neutral-300 bg-slate-500 dark:border-neutral-400 dark:bg-slate-800 sm:mt-16`}
 	>
-		<div
-			id="page-ruler-top"
-			class="mx-auto w-[120ch] justify-between border-x-2 border-neutral-700 bg-neutral-100"
-		/>
-		<!-- arrow handles -->
-		<div
-			class="mx-auto flex w-[120ch] -translate-y-3.5 items-center justify-between px-16 text-blue-900 dark:text-blue-400"
-		>
-			<PixelarticonsArrowBarRight class="h-4 w-4 -translate-x-[95%] -translate-y-[5%]" />
-			<PixelarticonsArrowBarLeft class="h-4 w-4 -translate-y-[5%] translate-x-[90%]" />
+		<div class="bg-neutral-300 px-2 pt-0.5 font-sans text-2xl dark:bg-neutral-800 sm:text-3xl">
+			<a
+				aria-label="Back to post list"
+				id="back-to-post-list"
+				class="-me-0.5 inline-block text-neutral-700 hover:text-blue-700 dark:text-neutral-400"
+				href="/p"
+				><span aria-hidden="true">/</span><span class="font-normal underline underline-offset-2"
+					>posts</span
+				><span aria-hidden="true">/</span></a
+			>
+			<span class="-ms-0.5 inline-block font-bold lowercase dark:text-neutral-400" id="slug"
+				>{post.slug}</span
+			>
 		</div>
-	</div>
 
-	<div class="flex w-full justify-center border-t-2 border-black">
-		<article
-			class="w-full max-w-[120ch] bg-neutral-50 p-4 dark:bg-neutral-900 sm:mx-6 sm:my-12 sm:border-x-4 sm:border-b-8 sm:border-t-4 sm:border-black sm:p-16"
+		<div class="flex justify-between bg-neutral-400 px-2 dark:bg-neutral-700">
+			<header>
+				<div class="flex w-full items-center text-lg">
+					<PixelarticonsInfoBox class="me-2 h-4 w-4 text-neutral-700" role="presentation" />
+					<div class="mt-1">
+						<span class="-mb-3 text-neutral-900 dark:text-neutral-200"
+							>Created: <time
+								class="font-bold text-neutral-900 dark:text-neutral-200"
+								datetime={dayjs(unfuckedDate).format('YYYY-MM-DD')}
+								>{dayjs(unfuckedDate).format('YYYY-MMM-DD')}</time
+							></span
+						>
+						<ul aria-label="post tags" class="flex">
+							{#if post.tags}
+								{#each post.tags as tag}
+									<li class="me-1 flex items-center text-lg text-neutral-900 dark:text-neutral-300">
+										<PixelarticonsLabel
+											role="presentation"
+											class="me-0.5 h-3 w-3 text-neutral-500 dark:text-neutral-400"
+										/><span>{tag}</span>
+									</li>
+								{/each}
+							{/if}
+						</ul>
+					</div>
+				</div>
+			</header>
+			<div class="mt-2 flex max-w-lg flex-col items-end text-neutral-900 dark:text-neutral-200">
+				<label>
+					Theme:
+					<select class="dark:bg-neutral-500" bind:value={theme}>
+						<option value="light">Light</option>
+						<option value="dark">Dark</option>
+					</select>
+				</label>
+			</div>
+		</div>
+		<!-- page margin ruler type thingy, purely decorative. -->
+		<div
+			aria-hidden="true"
+			class="hidden h-3 border-t-2 border-neutral-600 bg-neutral-300 dark:invert lg:block"
 		>
-			<slot />
-		</article>
+			<div
+				id="page-ruler-top"
+				class="mx-auto w-[120ch] justify-between border-x-2 border-neutral-700 bg-neutral-100"
+			/>
+			<!-- arrow handles -->
+			<div
+				class="mx-auto flex w-[120ch] -translate-y-3.5 items-center justify-between px-16 text-blue-900 dark:text-blue-400"
+			>
+				<PixelarticonsArrowBarRight class="h-4 w-4 -translate-x-[95%] -translate-y-[5%]" />
+				<PixelarticonsArrowBarLeft class="h-4 w-4 -translate-y-[5%] translate-x-[90%]" />
+			</div>
+		</div>
+
+		<div class="flex w-full justify-center border-t-2 border-black">
+			<article
+				class="w-full max-w-[120ch] bg-neutral-50 p-4 dark:bg-neutral-900 sm:mx-6 sm:my-12 sm:border-x-4 sm:border-b-8 sm:border-t-4 sm:border-black sm:p-16"
+			>
+				<slot />
+			</article>
+		</div>
 	</div>
 </div>
 
